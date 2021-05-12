ffb1f35525615dbcd08f5904ded119cb5a19f644
CauaneAndrade <cauane.emanuela@hotmail.com>
2021-05-12 18:38:26 -0300

    adicionado dados de teste para ser utilizado no chonkyno frontend

--- spaces-backend/src/controller/fileController.js
+++ spaces-backend/src/controller/fileController.js
@@ -79,6 +79,70 @@ router.get("/user/abacate", async (req, res) => {
     return res.json(data)
 });
 
+router.get("/user/abacate2", async (req, res) => {
+    file1 = await File.create({ name: 'arquivo1', isDir: false })
+    folder1_1 = await Folder.create({
+        name: 'pasta1_1',
+        isDir: true,
+        filhos: []
+    })
+    
+    folder1 = await Folder.create({
+        name: 'pasta1',
+        isDir: true,
+        filhos: [folder1_1]
+    })
+    
+    folder2 = await Folder.create({
+        name: 'pasta2',
+        isDir: true,
+        filhos: []
+    })
+    
+    vhost = await Folder({ name: '7f9b921d9ba0a8162bc6b0998d7dc05043da12a5', filhos: [], isDir: true })
+    vhost.filhos.push(folder1, folder2, file1)
+    vhost.save()
+    // ----------------------------------------------------------------------------------------
+    var dic = {
+        "rootFolderId": vhost._id,
+        "fileMap": {}
+    }
+    function getFilho(folder, parent) {
+        dic['fileMap'][folder.id] = {
+            "id": folder.id,
+            "name": folder.name,
+            "isDir": folder.isDir
+        }
+        dic['fileMap'][folder.id]["childrenIds"] = []
+        folder.filhos.forEach(obj => {
+            dic['fileMap'][folder.id]["childrenIds"].push(obj._id)
+        })
+        dic['fileMap'][folder.id]["childrenCount"] = folder.filhos.length
+        if (parent) {
+            dic['fileMap'][folder.id]["parentId"] = parent.id
+        }
+        folder.filhos.forEach(filho => {
+            if (filho.isDir) {
+                getFilho(filho, folder)
+            } else {
+                dic['fileMap'][filho._id] = {
+                    "id": filho.id,
+                    "name": folder.name,
+                }
+                if (folder) {
+                    dic['fileMap'][filho._id]["parentId"] = folder.id
+                }
+            }
+        })
+        // ----------------------------------------------------------------------------------------
+        console.log(dic)
+        return dic
+    }
+    
+    const data = await getFilho(vhost, '')
+    return res.json(data)
+});
+
 // upload de arquivo para o bucket
 router.post("/user/content", multer().single("file"), async (req, res) => {
     // isDir, path, name -> folder
